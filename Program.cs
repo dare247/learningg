eef4c27b5889dd06b8cd80ab777225f620e59cb6
dare247 <damilareoloruntobi4@gmail.com>
2024-06-04 10:56:31 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,46 +1,72 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
 Console.WriteLine("Hello, World!");
-
-Console.WriteLine("Calculator");
-Console.WriteLine("First Number: ");
-var num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Second Number: ");
-var num2 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Choose Operation: Add, Subtract, Multiplication, Division, Remainder, Exponent, Exit");
-var action = Console.ReadLine();
-switch (action)
+while (true)
 {
-    case "Add":
-        Console.WriteLine(num1 + num2);
-        break;
-    case "Subtract":
-        Console.WriteLine(num1 - num2);
-        break;
-    case "Multiplication":
-        Console.WriteLine(num1 * num2);
-        break;
-    case "Division":
-        if (num1 != 0 || num2 != 0)
-            Console.WriteLine(num1 / num2);
-        else
-            Console.WriteLine("Division cannot take place with a zero.");
-        break;
-    case "Remainder":
-        if (num1 > num2)
-            Console.WriteLine(num1 % num2);
-        else Console.WriteLine("Can't Implement");
-        break;
-    case "Exponent":
-        var result = 1;
-        for (int i = 0; i < num2; i++)
+    Console.WriteLine("Calculator");
+    Console.WriteLine("First Number: ");
+    var str1 = Console.ReadLine();
+    Console.WriteLine("Second Number: ");
+    var str2 = Console.ReadLine();
+
+    if (int.TryParse(str1, out _) && int.TryParse(str2, out _))
+    {
+        var num1 = Convert.ToInt32(str1);
+        var num2 = Convert.ToInt32(str2);
+        Console.WriteLine("Choose Operation: Add, Subtract, Multiplication, Division, Remainder, Exponent, Clear");
+        var action = Console.ReadLine();
+        
+        if(action == "Add" || action == "Subtract" || action == "Multiplication" || action == "Division" || action == "Remainder" || action == "Exponent" || action == "Clear")
         {
-            result = result * num1;
+            switch (action)
+            {
+                case "Add":
+                    Console.WriteLine(num1 + num2);
+                    break;
+                case "Subtract":
+                    Console.WriteLine(num1 - num2);
+                    break;
+                case "Multiplication":
+                    Console.WriteLine(num1 * num2);
+                    break;
+                case "Division":
+                    if (num1 != 0 || num2 != 0)
+                        Console.WriteLine(num1 / num2);
+                    else
+                        Console.WriteLine("Division cannot take place with a zero.");
+                    break;
+                case "Remainder":
+                    if (num1 > num2)
+                        Console.WriteLine(num1 % num2);
+                    else Console.WriteLine("Can't Implement");
+                    break;
+                case "Exponent":
+                    var result = 1;
+                    for (int i = 0; i < num2; i++)
+                    {
+                        result = result * num1;
+                    }
+                    Console.WriteLine(result);
+                    break;
+                case "Clear":
+                    break;
+            }
         }
-        Console.WriteLine(result);
-        break;
-    case "Exit":
-        break;
+        else
+        {
+            Console.WriteLine("Operation Not Part of possible operations");
+        }
+    }
+    else
+    {
+        Console.WriteLine("One of the inputs is not a Number.");
+    }
 }
+
+
+
+
 
 
